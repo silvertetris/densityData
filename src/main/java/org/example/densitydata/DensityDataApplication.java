bf53362fdc895b8faae1cb136ba2f5fde5b8fed9
silvertetris <dltjdrnjs3@naver.com>
2025-05-08 17:33:26 +0900

    first commit

---
+++ src/main/java/org/example/densitydata/DensityDataApplication.java
@@ -0,0 +1,13 @@
+package org.example.densitydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DensityDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DensityDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/densitydata/DensityDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.densitydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DensityDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
