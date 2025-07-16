d84efa9ac7b8621418d4d8c67a3d956ef98d6310
sean <dltjdrnjs3@naver.com>
2025-07-16 20:58:33 +0900

    lowercase

--- src/main/java/org/example/densitydata/service/Scheduler/TollgateDataScheduler.java
+++ src/main/java/org/example/densitydata/service/Scheduler/TollgateDataScheduler.java
@@ -1,4 +1,4 @@
-package org.example.densitydata.service.Scheduler;
+package org.example.densitydata.service.scheduler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
